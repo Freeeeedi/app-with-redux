b34d28622f85e05efb766c4a968f29244cfd371a
freeeeedi <egorjukovski@gmail.com>
2022-12-11 20:13:24 +0200

    Рефакторинг конмпонент Users и Post в классовые

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,17 @@
 import style from './Post.module.css';
+import { React } from 'react';
 
-const Post = (props) => {
-    return (
-        <div className={style.item}>
-            <img src="https://cdn1.iconfinder.com/data/icons/society-profession-avatar/1024/Society_Profession_Avatar_I-27-512.png" alt="" />
-            {props.text}
-            <div>
-                {props.likesCount > 1 ? props.likesCount + ' likes' : props.likesCount + ' like'}
+class Post extends React.Commponent {
+    render() {
+        return (
+            <div className={style.item}>
+                <img src="https://cdn1.iconfinder.com/data/icons/society-profession-avatar/1024/Society_Profession_Avatar_I-27-512.png" alt="" />
+                {this.props.text}
+                <div>
+                    {this.props.likesCount > 1 ? this.props.likesCount + ' likes' : this.props.likesCount + ' like'}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 export default Post;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,48 @@
 import style from './Users.module.css';
+import React from 'react';
 import axios from 'axios';
 import UserPhoto from '../../assets/images/user.jpg';
 
-
-let Users = (props) => {
-    if (props.users.length === 0) {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         });
     }
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id} className={style.user}>
-                    <div className={style.user_left}>
-                        <div className={style.user_img}>
-                            <img src={u.photos.small ? u.photos.small : UserPhoto} alt="" />
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id} className={style.user}>
+                        <div className={style.user_left}>
+                            <div className={style.user_img}>
+                                <img src={u.photos.small ? u.photos.small : UserPhoto} alt="" />
+                            </div>
+                            <div className={style.button}>
+                                {u.followed
+                                    ? <button onClick={() => { this.props.unfollow(u.id) }}>UNFOLLOW</button>
+                                    : <button onClick={() => { this.props.follow(u.id) }}>FOLLOW</button>
+                                }
+                            </div>
                         </div>
-                        <div className={style.button}>
-                            {u.followed
-                                ? <button onClick={() => { props.unfollow(u.id) }}>UNFOLLOW</button>
-                                : <button onClick={() => { props.follow(u.id) }}>FOLLOW</button>
-                            }
+                        <div className={style.user_right}>
+                            <div className={style.user_left_info}>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </div>
+                            <div className={style.user_right_info}>
+                                <div>{'u.location.country'}</div>
+                                <div>{'u.location.city'}</div>
+                            </div>
                         </div>
                     </div>
-                    <div className={style.user_right}>
-                        <div className={style.user_left_info}>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </div>
-                        <div className={style.user_right_info}>
-                            <div>{'u.location.country'}</div>
-                            <div>{'u.location.city'}</div>
-                        </div>
-                    </div>
-                </div>
-                )
-            }
+                    )
+                }
 
-        </div>
-    );
-};
+            </div>
+        );
+    }
+}
 
 export default Users;
